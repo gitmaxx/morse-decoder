d20f9f5bc1a6fafd3f2831044486319705cd34d2
Akulinichev Maxim <mxmrus@gmail.com>
2019-09-30 22:23:14 +0300

    morse-decoder/feature: implement morse decoder

--- src/index.js
+++ src/index.js
@@ -39,7 +39,39 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let currentLetter = '';
+    let decodedString = '';
+    let currentCode = '';
+    let currentSign = '';
+    let stringLength = expr.length;
+    let i= stringLength/10;
+    let k=1;
+    while (i-- > 0) {
+        currentCode = expr.substr((stringLength-i-1)*10, 10);
+        currentSign = currentCode.substr(8,2);
+        while (currentSign != '00' && currentSign != '**' ) 
+        {
+            if (currentSign = '11') {
+                currentLetter = '-' + currentLetter ;
+            } else {
+                currentLetter = '.' + currentLetter ;
+            };
+            ++k;
+            currentSign = currentCode.substr(10-2*k,2);
+        };
+        decodedString += MORSE_TABLE [currentLetter];
+        if (currentSign ='**') {decodedString += ' '};
+        currentSign ='';
+        currentLetter = '';
+        k=1;
+    };
+
+
+
+    return decodedString;
 }
+
+
 
 module.exports = {
     decode
